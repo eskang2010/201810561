d2b0379f4648880d217587b306366ab76aa2c14a
eskang2010 <145013873+eskang2010@users.noreply.github.com>
2023-09-21 21:10:22 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	int num1, num2;
+
+	printf("Input two integers:");
+	scanf_s("%d %d", &num1, &num2);
+
+	int num3 = num1&num2;
+	int num4 = num1 | num2;
+	int num5 = num1^num2;
+
+	printf("%d & %d = %d \n", num1, num2, num3);
+	printf("%d | %d = %d \n", num1, num2, num4);
+	printf("%d ^ %d = %d \n", num1, num2, num5);
+
+	return 0;
+}
